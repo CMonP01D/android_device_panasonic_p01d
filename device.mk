195680b48d5ae822219a81629a104aa179e03269
Hiroyuki Ikezoe <hiikezoe@gnome.org>
2013-03-25 20:45:47 +0900

    Enable touchpanel at boot.

--- device.mk
+++ device.mk
@@ -1,6 +1,8 @@
 PRODUCT_COPY_FILES += \
   device/panasonic/p01d/vold.fstab:system/etc/vold.fstab \
+  device/panasonic/p01d/root/init.qcom.rc:root/init.qcom.rc \
   device/panasonic/p01d/ueventd.qcom.rc:root/ueventd.qcom.rc \
+  device/panasonic/p01d/system/init.touchpanel.sh:system/etc/init.touchpanel.sh \
   device/panasonic/p01d/system/atmel-touchscreen.idc:system/usr/idc/atmel-touchscreen.idc
 
 PRODUCT_PACKAGES += \
